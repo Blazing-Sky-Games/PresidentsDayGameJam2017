d7b2845be8aeb44e6b36983a6e927807b522bdbd
Matthew Draper <draper_matthewjohn@yahoo.com>
2017-02-19 08:46:21 -0800

    remove unnecessary code copied from hero

--- Assets/Mob.cs
+++ Assets/Mob.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using MonsterLove.StateMachine;
 using DG.Tweening;
 public class Mob : MonoBehaviour
 {
@@ -17,18 +16,6 @@ public class Mob : MonoBehaviour
 	public AudioClip FireSound;
 	public AudioClip JumpSound;
 
-	// states for MonsterLove state machine
-
-	public enum HeroState
-	{
-		Idle,   // NOTE (matthew)
-				// right now there really isnt a difference between idle and walk, 
-				// but the are sperate so they can have different anims later
-		Walk,
-		Jump,
-		Fall,
-	}
-
 	void Awake()
 	{
 		// get components
@@ -42,13 +29,6 @@ public class Mob : MonoBehaviour
 		m_audio = GetComponent<AudioSource>();
 		m_tranGun = transform.FindChild("gun");
 		m_tranMuzzle = transform.FindChild("gun").FindChild("muzzle");
-	}
-
-	void Start()
-	{
-		// start in idle state
-
-		//m_fsm.ChangeState(HeroState.Idle);
 	}
 
 	// default root motion when nothing special is happening
@@ -274,166 +254,12 @@ public class Mob : MonoBehaviour
 			return true;
 	}
 
-	// idle state
-
-	void Idle_Enter()
-	{
-		// if we are idle, we are not moving in the y axis
-
-		m_vv = 0;
-	}
-
-	void Idle_Update()
-	{
-		UpdateVDefault();
-		UpdateGunDefault();
-
-		if (Input.GetButtonDown("Jump"))
-		{
-			// if we press the jump button, jump
-
-			m_fsm.ChangeState(HeroState.Jump);
-		}
-		else if (Input.GetAxisRaw("Horizontal") != 0)
-		{
-			// if there is horizontal input, start walking
-
-			m_fsm.ChangeState(HeroState.Walk);
-		}
-		else if (!Grounded())
-		{
-			// if we are not standing on ground, start falling
-
-			m_fsm.ChangeState(HeroState.Fall);
-		}
-	}
-
-	// walk state
-
-	void Walk_Enter()
-	{
-		// if we are walking we are not moving in the y axis 
-
-		m_vv = 0;
-	}
-
-	void Walk_Update()
-	{
-		UpdateVDefault();
-		UpdateGunDefault();
-
-		if (Input.GetButtonDown("Jump"))
-		{
-			// if we press jump, jump
-
-			m_fsm.ChangeState(HeroState.Jump);
-		}
-		else if (Input.GetAxisRaw("Horizontal") == 0)
-		{
-			// if we are not deflecting the stick, start idleing
-
-			m_fsm.ChangeState(HeroState.Idle);
-		}
-		else if (!Grounded())
-		{
-			// if we are not grounded, start falling
-
-			m_fsm.ChangeState(HeroState.Fall);
-		}
-	}
-
-	// jump state
-
-	void Jump_Enter()
-	{
-		m_audio.PlayOneShot(JumpSound);
-
-		// calculate launch velocity based on desired jump height
-
-		m_vv = Mathf.Sqrt(-2 * Physics2D.gravity.y * MaxJumpHeight);
-	}
-
-	void Jump_Update()
-	{
-		// acceleration due to gravity
-
-		m_vv += Physics2D.gravity.y * Time.deltaTime;
-
-		UpdateVDefault();
-		UpdateGunDefault();
-
-		if (Grounded() && m_vv < 0)
-		{
-			// this is here to handle landing on a ledge at the apex of a jump 
-			// if we are grounded the first frame our y velocity is negative just go to 
-			// idle instead of fall
-
-			// BB (matthew) is the needed?
-
-			m_fsm.ChangeState(HeroState.Idle);
-		}
-		else if (m_vv < 0 || !Input.GetButton("Jump"))
-		{
-			if (m_vv > 0)
-			{
-				// the behavior when you let go of jump while moving up is to have
-				// your upward velocity set to zero
-
-				// NOTE (matthew)
-				// this assumes we dont want the hero commited to a jump (i assume we do not)
-				// this behavior of setting the v to zero is the best feeling jump i have found,
-				// but if someone has a better idea im game
-
-				m_vv = 0;
-			}
-
-			// if we are not holding jump, or we have started moving down,
-			// start falling
-
-			m_fsm.ChangeState(HeroState.Fall);
-		}
-	}
-
-
-	// fall state
-
-	void Fall_Enter()
-	{
-		// replication of the behavior in Jump_Update that transisions to fall
-
-		// BB (matthew) should this only be in one place?
-
-		if (m_vv > 0)
-			m_vv = 0;
-	}
-
-	void Fall_Update()
-	{
-		// acceleration due to gravity
-
-		m_vv += Physics2D.gravity.y * Time.deltaTime;
-
-		UpdateVDefault();
-		UpdateGunDefault();
-
-		if (Grounded())
-		{
-			// if we land, start idleing
-
-			// BB (matthew) if we want a landing animation (not just jumping to idle), 
-			// we need extra logic here and in the idle_enter to handle that
-
-			m_fsm.ChangeState(HeroState.Idle);
-		}
-	}
-
 	// private state
 
 	private float m_vv;
 	private float m_vh;
 	private Rigidbody2D m_rb;
 	private BoxCollider2D m_col;
-	private StateMachine<HeroState> m_fsm;
 	private Transform m_tranMuzzle;
 	private Transform m_tranGun;
 	private float m_timeLastFire;
