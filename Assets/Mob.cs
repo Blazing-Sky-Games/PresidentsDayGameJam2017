948989fbda85b126fdbb563288fa027580295be9
Matthew Draper <draper_matthewjohn@yahoo.com>
2017-02-20 08:52:42 -0800

    Clean up mob script and fix bug where it would get stuck on left wall

--- Assets/Mob.cs
+++ Assets/Mob.cs
@@ -1,34 +1,19 @@
 ﻿using UnityEngine;
-using System.Linq;
 
 public class Mob : MonoBehaviour
 {
 	// editor properties
 
 	public float        WalkSpeed;
-
-    // bullet
-
-    public GameObject   BulletPrefab;
-	public float        BulletSpeed;
-	public float        BulletDelay;
-
-    // ai
-
-    public float        AttackRange;
-    public float        TimeBetweenBullets;
     public float        SensorRange;
     public LayerMask    PlayerLayer;
-	public LayerMask whatIsWall;
-
+	public LayerMask    WallLayer;
 
     void Awake()
 	{
 		// get components
 
 		m_rb = GetComponent<Rigidbody2D>();
-
-		m_tranGun = transform.FindChild("gun");
     }
 
     Vector2 DirectionToTarget()
@@ -39,49 +24,19 @@ public class Mob : MonoBehaviour
         }
         else
         {
-			return new Vector2(walkDirection, 0);
+			return new Vector2(0, 0);
         }
-    }
-
-    Vector3[] GetBulletPath()
-    {
-        return m_tranGun
-                .FindChild("Path")
-                .Cast<Transform>()
-                .Select(child => child.position)
-                .ToArray();
     }
 
 	RaycastHit2D WallOnLeft()
 	{
-		return Physics2D.Linecast(transform.position, (Vector2)transform.position + (Vector2.right * -1), whatIsWall);
+		return Physics2D.Linecast(transform.position, (Vector2)transform.position + (Vector2.right * -2), WallLayer);
 	}
 	RaycastHit2D WallOnRight()
 	{
-		return Physics2D.Linecast(transform.position, (Vector2)transform.position + (Vector2.right * 1), whatIsWall);
+		return Physics2D.Linecast(transform.position, (Vector2)transform.position + (Vector2.right * 2), WallLayer);
 	}
-	void UpdateAttack()
-    {
-        m_attackTimer -= Time.deltaTime;
-
-        if (m_targetCurrent != null && 
-            DirectionToTarget().magnitude < AttackRange &&
-            m_attackTimer < 0)
-        {
-            // reset the attack timer
-
-            m_attackTimer = TimeBetweenBullets;
-
-            // fire a bullet
-
-            //Vector3[] bulletPath = GetBulletPath();
-
-            //GameObject newBullet = Instantiate(BulletPrefab, bulletPath[0], Quaternion.identity);
-			
-		}
-    }
-	int walkDirection = 1;
-
+	
 	void Update()
 	{
         // clear target
@@ -101,18 +56,18 @@ public class Mob : MonoBehaviour
 
         if (DirectionToTarget().x < 0 || WallOnRight())
         {
-            walkDirection = -1;
+            m_walkDirection = -1;
         }
         else if (DirectionToTarget().x > 0 || WallOnLeft())
         {
-            walkDirection = 1;
+            m_walkDirection = 1;
         }
 
-        m_vh = WalkSpeed * walkDirection;
+        m_vh = WalkSpeed * m_walkDirection;
 
         // make sure the mob is facing the right direction
 
-        if (walkDirection < 0)
+        if (m_walkDirection < 0)
         {
             transform.localScale = new Vector3(-1, 1, 1);
         }
@@ -121,24 +76,12 @@ public class Mob : MonoBehaviour
             transform.localScale = new Vector3(1, 1, 1);
         }
 
-        // rotate the gun to point at the target
-
-        if (m_targetCurrent != null)
-        {
-            float angleToTarget = Mathf.Atan2(DirectionToTarget().y, DirectionToTarget().x) * Mathf.Rad2Deg;
-            m_tranGun.rotation = Quaternion.AngleAxis(walkDirection < 0 ? angleToTarget - 180 : angleToTarget, Vector3.forward);
-        }
-
-        // attack if we can
-
-        UpdateAttack();
-
         // set rb velocity
 
         m_rb.velocity = new Vector2(m_vh, m_rb.velocity.y);
     }
 
-    // debug drawing of sesor range and attack range
+    // debug drawing of sesor range
 
     void OnDrawGizmos()
 	{
@@ -160,16 +103,6 @@ public class Mob : MonoBehaviour
 			Gizmos.DrawLine(transform.position, m_targetCurrent.position);
 			Gizmos.DrawLine(transform.position, transform.position + Vector3.right * -1);
 			Gizmos.DrawLine(transform.position, transform.position + Vector3.right * 1);
-
-			if (DirectionToTarget().magnitude < AttackRange)
-			{
-				Gizmos.color = Color.green;
-			}
-
-            // draw a circle for the attack range
-            // red if the target is not in range, green if it is
-
-			Gizmos.DrawWireSphere(transform.position, AttackRange);
 		}
 
         // restore the old gizmo color
@@ -181,8 +114,7 @@ public class Mob : MonoBehaviour
 
     private Rigidbody2D     m_rb;
     private Transform       m_tranGun;
-
     private float           m_vh;
-    private float           m_attackTimer = 1;
     private Transform       m_targetCurrent;
+    int                     m_walkDirection = 1;
 }
